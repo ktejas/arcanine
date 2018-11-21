db5a9e00919a9b1b46f377916234f8c293b19323
ktejas <krishnasalimeda@gmail.com>
2018-11-21 19:01:07 +0530

    Added sprite bike shadow. Needs rework

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject goBikeShadow;
+
+	void Start ()
+    {
+        //goBikeShadow.transform.worldRotation = Quaternion.identity;
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
